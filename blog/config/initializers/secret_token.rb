8ac0800b91e68ccc6d98e4693789feaf0e7a3789
Webonise <webonise@webonise-desktop.(none)>
2012-12-18 18:52:06 +0530

    file removed

--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '70ed0e52b381f60f6c7147af434869867214e53c1e70079e88f827c49fd494ab7ae682ce6b9bf35fd22c428d985b366e7e80195290af170cab302736ea2053e5'
